020ab2ff62a266956b6e707c7b3bb0ce11a1c9da
markwright2002 <mark.wright8@ontariotechu.net>
2022-09-16 22:35:19 -0400

    added moving obstacles and destroy them after 10 seconds

--- Assets/obstacle.cs
+++ Assets/obstacle.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class obstacle : MonoBehaviour
 {
+
+    public Vector2 vec;
+    public float speed, x;
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        vec.x = 0;
+        vec.y = -1;
+
+        transform.position += (Vector3)vec * speed * Time.deltaTime;
+
+        x += Time.deltaTime;
+        if (x > 10)
+        {
+            Destroy(gameObject);
+        }
         
     }
 }
--- Assets/spawn.cs
+++ Assets/spawn.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class spawn : MonoBehaviour
 {
+    public GameObject obs;
+    public float x, y, z;
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
+        x += Time.deltaTime;
+        if (x > 2.0)
+        {
+            GameObject clone1;
+
+            clone1 = Instantiate(obs, new Vector3(Random.Range(y, z), transform.position[1], transform.position[2]), transform.rotation);
+
+            //InstantiatePrefab();
+
+            x = 0;
+        }
     }
 }
